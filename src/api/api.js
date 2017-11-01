58995a1cc6841f6ae6ba927f5ab413dde541241c
Michael Chen <maochen201411@gmail.com>
2017-11-01 20:09:29 +0800

    embed api step 2 on the way

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@
  * @Project: one_server
  * @Filename: api.js
  * @Last modified by:   mymac
- * @Last modified time: 2017-11-01T13:51:20+08:00
+ * @Last modified time: 2017-11-01T17:09:06+08:00
  */
 
 import {
